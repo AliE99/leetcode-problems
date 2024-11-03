c9badf58c66d79abef5ca313bea495704ea1c543
Ali Elahi <alielahi.1378@gmail.com>
2024-11-03 06:53:11 +0330

    Better solution for the Merge Intervals

--- Medium/056_merge_intervals.py
+++ Medium/056_merge_intervals.py
@@ -6,16 +6,12 @@ class Solution:
         intervals = sorted(intervals, key=lambda x: x[0])
         i = 0
         while i < len(intervals) - 1:
-            if (intervals[i][-1] >= intervals[i + 1][0]) and (
-                intervals[i + 1][-1] >= intervals[i][0]
-            ):
-                intervals[i + 1] = [
-                    min(intervals[i][0], intervals[i + 1][0]),
-                    max(intervals[i][-1], intervals[i + 1][-1]),
-                ]
-                del intervals[i]
+            if intervals[i][-1] >= intervals[i + 1][0]:
+                intervals[i][1] = (max(intervals[i][-1], intervals[i + 1][-1]),)
+                del intervals[i + 1]
             else:
                 i += 1
+
         return intervals
 
 
