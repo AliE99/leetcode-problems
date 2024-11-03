37368a91a28bb05ffb4df4e6db1f18fbef17f6ba
Ali Elahi <alielahi.1378@gmail.com>
2024-11-03 06:53:11 +0330

    Valid Anagram

---
+++ Easy/242_valid_anagram.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        if set(s) != set(t):
+            return False
+
+        map_s = {}
+        map_t = {}
+        for i in range(0, len(s)):
+            if s[i] in map_s:
+                map_s[s[i]] += 1
+            else:
+                map_s[s[i]] = 1
+
+            if t[i] in map_t:
+                map_t[t[i]] += 1
+            else:
+                map_t[t[i]] = 1
+
+        if map_s != map_t:
+            return False
+
+        return True
+
+
+s = "anagram"
+t = "nagaram"
+
+s = "aacc"
+t = "ccac"
+
+print(Solution().isAnagram(s, t))
