eee8a7cf45833230f82822b85f7aa6421efc3296
Ali Elahi <alielahi.1378@gmail.com>
2025-02-13 19:23:22 +0330

    Feat: A better solution for the Add Two Numbers problem

--- Algorithms/Medium/002_add_two_numbers.py
+++ Algorithms/Medium/002_add_two_numbers.py
@@ -29,6 +29,30 @@ class Solution:
         return int(number)
 
 
+class BetterSolution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            sum_val = carry
+            if l1:
+                sum_val += l1.val
+                l1 = l1.next
+
+            if l2:
+                sum_val += l2.val
+                l2 = l2.next
+
+            carry, digit = divmod(sum_val, 10)
+            current.next = ListNode(digit)
+            current = current.next
+
+        return dummy.next
+
+
 # Function to create a linked list from a list
 def create_linked_list(arr):
     if not arr:  # if the list is empty, return None
